81365b17c727f56f7f456695f8342c620ecc38e6
ucLh <luchwork@mail.ru>
2020-11-13 09:18:41 +0300

    Added some commentaries to the application example.

--- application/segmentation/main.cpp
+++ application/segmentation/main.cpp
@@ -9,20 +9,24 @@ using namespace std;
 int main() {
   cv::Mat img = cv::imread("left_2_000000318.jpg");
 
+  // Create wrapper.
   SegmentationWrapper seg_wrapper;
 
-//  seg_wrapper.loadFromCudaEngine(
-//      "effnetb0_unet_gray_2grass_iou55_640x1280.bin");
+  // Prepare wrapper for inference. You have to give a path to the config.
   seg_wrapper.prepareForInference("config.json");
+  // Check if network loaded.
   std::cerr << "Status of load: " << seg_wrapper.getLastError() << std::endl;
 
+  // Inference an image.
   seg_wrapper.inference(img);
   std::cerr << "Status of inference: " << seg_wrapper.getLastError()
             << std::endl;
+  // Get index or color mask via corresponding method
   cv::imwrite("1_trt_index.jpg", seg_wrapper.getIndexMask());
   cv::imwrite("1_trt_color.jpg", seg_wrapper.getColorMask(0.4, img));
   cv::waitKey(0);
 
+  // Time measurement. Inference one picture over and over again.
   auto t1 = std::chrono::high_resolution_clock::now();
 
   std::cout << "Starting inference TRT..." << std::endl;
@@ -32,7 +36,8 @@ int main() {
     seg_wrapper.getIndexMask();
     auto t1_2 = std::chrono::high_resolution_clock::now();
     auto duration =
-        std::chrono::duration_cast<std::chrono::microseconds>(t1_2 - t1_1).count();
+        std::chrono::duration_cast<std::chrono::microseconds>(t1_2 - t1_1)
+            .count();
     std::cout << "Inference + postprocessing took: " << duration << "\n";
   }
 
